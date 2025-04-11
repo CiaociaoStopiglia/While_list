9a2e0b438e4d0d2eb3803b504c27ef9c2270a1d5
João Stopiglia <stopiglia.design@gmail.com>
2025-04-11 10:56:34 -0300

    exercise 2

--- Exercise_1.js
+++ Exercise_1.js
@@ -4,3 +4,7 @@ while (numero <= 10){
     console.log("Volta número:", numero);
     numero++;
 }
+
+//imprime todos os números de 1 a 10
+//para mudar basta mudar o numero 10 em "(numero <= 10)"
+// João Stopiglia (@CiaociaoStopiglia) - 11/04/25
---
+++ Exercise_2.js
@@ -0,0 +1,11 @@
+let numero = 1
+
+while(numero <=15){
+   if (numero % 2 !== 0.) {
+    console.log(numero)
+   }
+    numero++
+}
+
+//imprime números ímpares
+// João Stopiglia (@CiaociaoStopiglia) - 11/04/25
