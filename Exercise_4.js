cbb6db8bb6322f253ade94bb310f2556a74efd34
João Stopiglia <stopiglia.design@gmail.com>
2025-04-11 11:24:38 -0300

    Exercicio 4

---
+++ Exercise_4.js
@@ -0,0 +1,9 @@
+let numero = 0;
+
+while (numero < 10) {
+    numero++
+    console.log(`a multiplicação é ${numero} x 5 = ${numero * 5}`)
+}
+
+//observação: caso queria deixar só a multiplicação, retire o "a multiplicação é..."
+// João Stopiglia (@CiaociaoStopiglia) - 11/04/25
