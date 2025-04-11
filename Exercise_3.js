f47c28c2d932c482a7c547e362b4745be82ce7b3
João Stopiglia <stopiglia.design@gmail.com>
2025-04-11 11:35:01 -0300

    Exercicio 4

---
+++ Exercise_3.js
@@ -0,0 +1,11 @@
+let soma = 0
+let contador = 1
+
+console.log("somando ate chegar ou passar de 40")
+while(soma <=40){
+    soma += contador
+    console.log(contador + " + " +soma)
+    contador++
+}
+
+console.log ("usei " + (contador - 1) + " numeros!")
